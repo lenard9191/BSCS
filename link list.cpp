a9f777b78c7f1bbc851749abaff58b6dcd660f69
lenard9191 <102806887+lenard9191@users.noreply.github.com>
2022-10-14 10:52:56 +0800

    Add files via upload

---
+++ link list.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+struct link
+{
+    int value;
+    link* next;
+};
+
+int main()
+{
+    int count = 3;
+    link* head = new link;
+    link * scan = head;
+
+    for(int x = 1; x <= count; x++)
+    {
+
+        scan->value = x;
+
+        if (x != count) {
+
+            scan->next = new link;
+            assert (scan->next != NULL);
+        }
+        else {
+            scan->next = head;
+            break;
+        }
+
+        scan = scan->next;
+
+    }
+    link* temp = head;
+    temp = head;
+    for (int x = 1; x <= 10;  x++) {
+        cout << temp << ";" <<temp->value<<  " \n" ;
+        temp = temp->next;
+
+    }
+
+    int i = 0;
+    scan->next = NULL;
+    scan = head;
+    while ( scan){
+        link *temp1 = scan;
+        scan = scan->next;
+        delete temp1;
+
+        i++;
+
+    }
+
+    /*scan = head;
+    for (int x = 0; x < count; x++) {
+        node * nextNode = scan->next;
+        delete scan;
+        scan = nextNode;
+    }
+
+    */
+
+    cout << "\ndone " << i << " Times" << endl;
+
+	return 0;
+}
